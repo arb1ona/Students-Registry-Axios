2dd1db0617882a9b6b54037d2c7bc66dd4cfe33b
arb1ona <arbiona.shahu@gmail.com>
2020-12-24 00:07:07 +0100

    MongoDB established successfully

--- students-registry/backend/server.js
+++ students-registry/backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -10,8 +11,18 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
-// this is going to allow us parse JSON 'cuz our server
-// is going to sending and receiving JSON
+// this is going to allow us parse JSON 'cuz our server is going to sending and receiving JSON
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established succesfully!");
+});
 
 // starts the server on a certain port
 app.listen(port, () => {
