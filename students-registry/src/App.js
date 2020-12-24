584f3f96cacb551e6d7bc12f5aac748ee9912d92
arb1ona <arbiona.shahu@gmail.com>
2020-12-24 20:01:00 +0100

    Setup Frontend Components

--- students-registry/src/App.js
+++ students-registry/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import tree from "./tree.jpg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ExercisesList from "./components/ExercisesList";
+import EditExercise from "./components/EditExercise";
+import CreateUser from "./components/CreateUser";
+import CreateExercise from "./components/CreateExercise";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
