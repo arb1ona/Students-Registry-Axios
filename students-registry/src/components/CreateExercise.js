3b9e45a6cbcc693d1f2c5b6bf4d78bb1f0ea07ac
arb1ona <arbiona.shahu@gmail.com>
2020-12-25 22:31:44 +0100

    Create User Tested

--- students-registry/src/components/CreateExercise.js
+++ students-registry/src/components/CreateExercise.js
@@ -58,8 +58,8 @@ export default class CreateExercise extends Component {
       duration: this.state.duration,
       date: this.state.date,
     };
-
-    console.log('onSubmit : "', this.state);
+    console.log(exercise);
+    //console.log('onSubmit : "', this.state);
 
     // window.location = "/";
   }
--- students-registry/src/components/CreateUser.js
+++ students-registry/src/components/CreateUser.js
@@ -1,7 +1,59 @@
 import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
 export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+    };
+  }
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+    console.log(user);
+
+    this.setState({
+      username: "",
+    });
+  }
+
   render() {
-    return <div>Create User</div>;
+    return (
+      <div>
+        Create User
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group>
+            <Form.Label>Username: </Form.Label>
+            <Form.Control
+              type="text"
+              required
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </Form.Group>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </Form>
+      </div>
+    );
   }
 }
